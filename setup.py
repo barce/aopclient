c0757e14447ce9685c83127905ee185969ca5fd9
Barce <james.barcelona@accuenmedia.com>
2017-05-17 16:43:43 -0700

    updated setup and added manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aopclient',
-    version='0.0.1',
+    version='0.0.2',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['aopclient', 'aopclient.tests'],
@@ -13,6 +13,6 @@ setup(
     url='http://pypi.python.org/pypi/aopclient/',
     license='LICENSE',
     description='A client for interacting with the AOL Platform.',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
 )
  
