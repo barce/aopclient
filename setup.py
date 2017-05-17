d2353fbb69d34ee4518f8db1eba92a4dc49fa548
Barce <james.barcelona@accuenmedia.com>
2017-05-17 16:45:25 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aopclient',
-    version='0.0.2',
+    version='0.0.3',
     author='Jim Barcelona',
     author_email='barce@me.com',
     packages=['aopclient', 'aopclient.tests'],
