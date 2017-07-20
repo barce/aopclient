47aa1556477f881bb9463c7c0e8662b6b14b7f0c
Barce <james.barcelona@accuenmedia.com>
2017-07-20 16:32:32 -0700

    adding flights

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aopclient',
-    version='0.0.8',
+    version='0.0.9',
     author='Jim Barcelona, Arun Suresh',
     author_email='barce@me.com, arunvsuresh@gmail.com',
     packages=['aopclient', 'aopclient.tests'],
