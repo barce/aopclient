26a942a5f97e1791c0fed7212e00a40b6cd9cf32
Barce <james.barcelona@accuenmedia.com>
2017-07-21 10:09:05 -0700

    remove comments

--- aopclient/aolclient.py
+++ aopclient/aolclient.py
@@ -159,15 +159,13 @@ def unassign_deal_assignments(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=
     print("{}".format(json.loads(response.text)))
     return json.loads(response.text)
 
-# GET 
-# /campaign­management/v1/organizations/{organizationId}//advertisers/{advertiserId}/campaigns/{campaignId}/tactics/{tacticId}/flights/{flightManagementId}
+
   def get_flight_by_id(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, flight_id=0):
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/flights/{5}".format(self.one_host, org_id, ad_id, campaign_id, tactic_id, flight_id)
     response = requests.get(url, headers=self.authorized_headers, verify=False)
     return json.loads(response.text)
 
-# GET 
-# /campaign­management/v1/organizations/{organizationId}/advertisers/{advertiserId}/campaign s/{campaignId}/tactics/{tacticId}/flights 
+
   def get_flights_by_tactic_id(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0):
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/flights".format(self.one_host, org_id, ad_id, campaign_id, tactic_id)
     response = requests.get(url, headers=self.authorized_headers, verify=False)
