2fb9454ec4c3b530620e4e0dd925a45efb252d17
Barce <james.barcelona@accuenmedia.com>
2017-10-06 18:35:13 -0700

    blacklists by tactic

--- aopclient/aolclient.py
+++ aopclient/aolclient.py
@@ -187,6 +187,13 @@ class AOLClient:
     response = self._send_request(url, self.authorized_headers, method="GET")
     return json.loads(response.text)
 
+def get_blacklists_by_tactic(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, limit=0, offset=0):
+    url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/blacklists".format(self.one_host, org_id, ad_id, campaign_id, tactic_id, limit, offset)
+    if limit > 0 or offset > 0:
+        url = url + "?limit={0}&offset={1}".format(limit, offset)        
+    response = self._send_request(url, self.authorized_headers, method="GET")
+    return json.loads(response.text)
+
   def get_creatives_by_tactic(self, org_id=0, ad_id=0, campaign_id=0, tactic_id=0, limit=0, offset=0):
     url = "https://{0}/advertiser/campaign-management/v1/organizations/{1}/advertisers/{2}/campaigns/{3}/tactics/{4}/creativeassignments".format(self.one_host, org_id, ad_id, campaign_id, tactic_id, limit, offset)
     if limit > 0 or offset > 0:
@@ -207,6 +214,7 @@ class AOLClient:
         url = url + "?limit={0}&offset={1}".format(limit, offset)        
     response = self._send_request(url, self.authorized_headers, method="GET")
     return json.loads(response.text)
+
 
       
   def _send_request(self, url, headers, data=None, method="GET"):
