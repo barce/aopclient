e01076ead773cdcd2232b884c2dd4adbb92bd1c9
Barce <james.barcelona@accuenmedia.com>
2017-11-30 17:52:13 -0800

    updated driver.py

--- scripts/driver.py
+++ scripts/driver.py
@@ -5,6 +5,13 @@ client = AOLClient()
 client.show_config()
 client.connect()
 client.get_organizations()
-client.get_campaigns(7000095690)
-client.get_campaigns_by_advertiser(7000095690, 7000095690)
-client.get_campaigns_by_advertiser_by_campaign(7000095690, 7000095690, 131210756)
+# client.get_campaigns(7000095690)
+# client.get_campaigns_by_advertiser(7000095690, 7000095690)
+# client.get_campaigns_by_advertiser_by_campaign(7000095690, 7000095690, 131210756)
+
+name = 'two domains'
+domains = ['https://www.google.com', 'https://slashdot.org']
+apps = []
+default = False
+result = client.create_whitelist_by_advertiser(7000095690, 7000095690, name, domains, apps, default)
+print(result)
