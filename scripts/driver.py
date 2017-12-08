8d80308d98b0432e84205752becccdb0e5693f91
Barce <james.barcelona@accuenmedia.com>
2017-12-07 16:46:48 -0800

    blacklist script for driver

--- scripts/driver.py
+++ scripts/driver.py
@@ -40,3 +40,37 @@ value = 'updated whitelist - {}'.format(now)
 result = client.update_whitelist_by_advertiser(7000095690, 7000095690, whitelist_id, op, path, value)
 print(result)
 
+print(' ')
+print(' ')
+print(' ')
+
+print('------ create blacklist ------')
+print(' ')
+now = datetime.datetime.now()
+name = 'two domains - {}'.format(now)
+domains = ['https://www.spam.la', 'https://zynga.com']
+apps = []
+default = False
+result = client.create_blacklist_by_advertiser(7000095690, 7000095690, name, domains, apps, default)
+print(result)
+print('------ create blacklist ------')
+print(' ')
+print(' ')
+print(' ')
+
+print('------ update blacklist ------')
+print(' ')
+# get id and update
+now = datetime.datetime.now()
+blacklist_id = result['blacklist']['id']
+op = 'REPLACE'
+path = '/name'
+value = 'updated blacklist - {}'.format(now)
+#   def update_blacklist_by_advertiser(self, org_id=0, ad_id=0, blacklist_id=0, op='REPLACE', path='/name', value=''):
+result = client.update_blacklist_by_advertiser(7000095690, 7000095690, blacklist_id, op, path, value)
+print(result)
+
+print(' ')
+print(' ')
+print(' ')
+
